20d02a2b798037b11537aae30125b8e5f0cb2039
mattsaeeda <mattsaeeda@gmail.com>
2019-11-04 23:02:19 -0500

    User contract deleted, new functions added to Perligo.sol

--- contracts/Perligo.sol
+++ contracts/Perligo.sol
@@ -2,23 +2,28 @@ pragma solidity >=0.5.0;
 
 import "./Rewards.sol";
 
-contract Perligo  {
+contract Perligo is Rewards {
+
+      uint256 public tokensPerBlock; 
+      uint256 public blockFreezeInterval; 
+
+      constructor() Rewards(tokensPerBlock , blockFreezeInterval){}
 
       /**
-      * @dev Function to add users in the network. 
+      * @dev Function to add users to the economy. 
       * @param _user The address that will be able to mint tokens.
       * @return A boolean that indicates if the operation was successful.
       */
-      function addUser(address _user) oublic returns (bool){
+      function addUser(address _user) public returns (bool){
 
       }
 
       /**
-      * @dev Function to remove users in the network. 
+      * @dev Function to remove users from the economy. 
        * @param _user The address that will be unable to mint tokens.
       * @return A boolean that indicates if the operation was successful.
       */
-      function removeUsers(address _user) public returns (bool) {
+      function removeUser(address _user) public returns (bool) {
       }
       
       /**
@@ -26,5 +31,25 @@ contract Perligo  {
       * @return A boolean that indicates if the operation was successful.
       */
       function mintTokensToPool() private returns (bool) {
+      }
+
+      function updateUserMask(address _user) private returns (bool){
+
+      }
+
+      function withdrawReward(address _user) private returns (bool){
+            
+      }
+
+      function showUserMask(address _user) public returns (bool){
+
+      }
+
+      function updateTokensPerBlock() private returns (bool){
+
+      }
+
+      function updateBlockFreezeInterval() private returns (bool){
+
       }
 }
